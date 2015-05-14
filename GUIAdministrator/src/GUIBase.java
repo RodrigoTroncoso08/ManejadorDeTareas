ba5f63eae081c1446cad26f0ef9d650695040fc7
dasenRS <superdasen@gmail.com>
2015-05-14 16:40:23 +0100

    el panel que hice lo pase al main, dentro de otro panel y con escondido hasta que se haga click en una tarea...falta todavia

--- GUIAdministrator/src/GUIBase.java
+++ GUIAdministrator/src/GUIBase.java
@@ -16,8 +16,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JScrollPane;
 import javax.swing.JComboBox;
@@ -43,11 +47,13 @@ import javax.swing.BoxLayout;
 
 
 
+
 import sun.java2d.loops.DrawLine;
 
 import java.awt.BasicStroke;
 import java.awt.Component;
 import java.awt.Dimension;
+
 
 
 
@@ -67,18 +73,21 @@ import java.awt.Insets;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
+
+
 
 import javax.swing.JList;
 import javax.swing.JScrollBar;
+
+
 public class GUIBase {
 	private JFrame frame;
 	private JTextField txtSearch;
 	private Administrator admin;
 	private ArrayList<ProyectPanel> ProyectUI = new ArrayList<ProyectPanel>();
 	RoundedPanel WhiteBase = new RoundedPanel();
+	RoundedPanel TaskDetail = new RoundedPanel();
+	RoundedPanel WhiteBase2 = new RoundedPanel();
 	JPanel GlosaryPanel = new JPanel();
 	private JLabel Titulo;
 	/**
@@ -506,12 +515,10 @@ public class GUIBase {
 		comboBox.setEditable(true);
 		comboBox.setFont(new Font("Arial Rounded MT Bold", Font.BOLD, 12));
 		comboBox.setForeground(new Color(0, 204, 255));
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"       Miselaneo"}));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Miselaneo"}));
 		comboBox.setSelectedIndex(0);
 		comboBox.setBackground(new Color(255, 255, 255));
 		comboBox.setBounds(25, 172, 147, 28);
-		comboBox.addItem("Miselaneo");
-		comboBox.addItem("patatas");
 		MenuPanel.add(comboBox);
 		
 		JSeparator separator = new JSeparator();
@@ -563,8 +570,118 @@ public class GUIBase {
 		Titulo.setBounds(276, 17, 422, 40);
 		frame.getContentPane().add(Titulo);
 		
+		WhiteBase2.setBounds(6, 69, 1005, 576);
+		WhiteBase2.setBackground(new Color(255,255,255));
+		WhiteBase2.add(TaskDetail);
+		WhiteBase2.setLayout(null);
+		frame.getContentPane().add(WhiteBase2);
+		
+		//aca viene la parte de los detalles de las tareas, donde se pueden editar y todo eso
+		//es un panel azul que deberia ir a la derecha de la pantalla y se hace visible solo
+		//cuando se selecciona una tarea
+		
+		TaskDetail.setBackground(new Color(30, 144, 255));
+		TaskDetail.setForeground(new Color(212, 227, 252));
+		TaskDetail.setBounds(640, 22, 350, 545);
+		TaskDetail.setLayout(null);
 		
 		
+		JTextArea textArea = new JTextArea();
+		textArea.setBackground(new Color(102, 205, 170));
+		textArea.setBounds(30, 100, 290, 210);
+		TaskDetail.add(textArea);
+		
+		JLabel lblNombreTarea = new JLabel("Nombre Tarea");
+		lblNombreTarea.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNombreTarea.setForeground(new Color(255, 255, 255));
+		lblNombreTarea.setBounds(73, 44, 200, 50);
+		TaskDetail.add(lblNombreTarea);
+		
+		JComboBox ctx = new JComboBox();
+		ctx.setBounds(30, 344, 90, 20);
+		TaskDetail.add(ctx);
+		
+		JComboBox impo = new JComboBox();
+		impo.setBounds(200, 28, 120, 20);
+		impo.setModel(new DefaultComboBoxModel(new String[] {"Normal","Importante","Muy Importante"}));
+		TaskDetail.add(impo);
+		
+		JCheckBox chckbxTareaLista = new JCheckBox("Tarea Lista");
+		chckbxTareaLista.setBackground(new Color(30, 144, 255));
+		chckbxTareaLista.setBounds(30, 487, 97, 23);
+		chckbxTareaLista.setBorder(null);
+		TaskDetail.add(chckbxTareaLista);
+		
+		JLabel lblProceso = new JLabel("Progreso");
+		lblProceso.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblProceso.setBounds(29, 510, 69, 23);
+		TaskDetail.add(lblProceso);
+		
+		JSlider slider = new JSlider();
+		slider.setBackground(new Color(30, 144, 255));
+		slider.setForeground(new Color(64, 224, 208));
+		slider.setBounds(61, 544, 200, 20);
+		TaskDetail.add(slider);
+		
+		JButton btnD = new JButton("+ D");
+		btnD.setBounds(180, 385, 51, 23);
+		TaskDetail.add(btnD);
+		
+		JButton btnW = new JButton("+ W");
+		btnW.setBounds(251, 385, 69, 23);
+		TaskDetail.add(btnW);
+		
+		JTextField txtHh = new JTextField();
+		txtHh.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent arg0) {
+				if(txtHh.getText().equals("HH"))
+					txtHh.setText("");
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(txtHh.getText().equals(""))
+					txtHh.setText("HH");				
+			}
+		});
+		txtHh.setForeground(new Color(255, 255, 255));
+		txtHh.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		txtHh.setBackground(new Color(30, 144, 255));
+		txtHh.setText("HH");
+		txtHh.setBounds(180, 431, 43, 34);
+		TaskDetail.add(txtHh);
+		txtHh.setColumns(10);
+		
+		JTextField txtMm = new JTextField();
+		txtMm.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if(txtMm.getText().equals("MM"))
+					txtMm.setText("");
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(txtMm.getText().equals(""))
+					txtMm.setText("MM");
+			}
+		});
+		txtMm.setBackground(new Color(30, 144, 255));
+		txtMm.setForeground(new Color(255, 255, 255));
+		txtMm.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		txtMm.setText("MM");
+		txtMm.setBounds(228, 431, 43, 34);
+		TaskDetail.add(txtMm);
+		txtMm.setColumns(10);
+		
+		JButton btnGuardar = new JButton("GUARDAR");
+		btnGuardar.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnGuardar.setBackground(new Color(0, 255, 0));
+		btnGuardar.setBounds(203, 476, 117, 38);
+		TaskDetail.add(btnGuardar);
+		//y hasta aca llegan los componentes del panel
+		//como no se puede hacer regiones en eclipse
+		//lo marco con comentarios
+		WhiteBase2.setVisible(false);
 		
 		
 	}
